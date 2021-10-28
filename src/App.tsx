bca885f7311960cc430bf9aa9b8ae37619a9a17e
JessicaLopesDev <jessicalopes.dev@gmail.com>
2021-10-28 09:28:29 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import Routes from './Routes';
+import GlobalStyles from './styles/GlobalStyles';
+import { useTheme } from './hooks/theme';
+
+
+const App: React.FC = () => {
+  const { theme } = useTheme();
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Routes />
+    </ThemeProvider>
+  )
+}
+
+export default App;
